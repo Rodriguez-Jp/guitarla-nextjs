f66c16ef99ac610b9022536d12f797edc0914ee8
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-07-26 22:47:56 -0500

    Se termina la pantalla de inicio

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
 import Layout from "@/components/layout";
+import Link from "next/link";
+import styles from "@/styles/home.module.css";
+import Curso from "@/components/curso";
 
-export default function Home() {
+export default function Home({ curso }) {
   return (
     <>
       <Layout
         title={"Inicio"}
         description={"Blog de musica, venta de guitarras y mas!"}
       >
-        <h1>Hola mundo en next</h1>
+        <main className="contenedor">
+          <h1 className="heading">Bienvenido a nuestra tienda de guitarras</h1>
+          <div className={styles["seccion-botones"]}>
+            <Link href={"/tienda"} className={styles.boton}>
+              Tienda
+            </Link>
+            <Link href={"/blog"} className={styles.boton}>
+              {" "}
+              Blog
+            </Link>
+          </div>
+        </main>
+
+        <section>
+          <Curso curso={curso.attributes} />
+        </section>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const respuesta = await fetch(`${process.env.API_URL}/curso?populate=imagen`);
+  const { data: curso } = await respuesta.json();
+
+  return {
+    props: {
+      curso,
+    },
+  };
 }
